d62cac31510cdfd6b47d5f44260c81c162365ab6
Lisandro Jiménez <lisandro.25.lj@gmail.com>
2024-11-30 16:42:57 -0300

    Redireccion a las modificaciones

--- app/src/main/java/com/example/tpf_paii_android/ModificarEstudiante.java
+++ app/src/main/java/com/example/tpf_paii_android/ModificarEstudiante.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ModificarEmpleado extends AppCompatActivity {
+public class ModificarEstudiante extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_modificar_empleado);
+        setContentView(R.layout.activity_modificar_estudiante);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/tpf_paii_android/actividades/menu_header/PerfilActivity.java
+++ app/src/main/java/com/example/tpf_paii_android/actividades/menu_header/PerfilActivity.java
@@ -3,20 +3,23 @@ package com.example.tpf_paii_android.actividades.menu_header;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tpf_paii_android.ModificarContrasena;
+import com.example.tpf_paii_android.ModificarEmpresa;
+import com.example.tpf_paii_android.ModificarEstudiante;
+import com.example.tpf_paii_android.ModificarTutor;
 import com.example.tpf_paii_android.MyApp;
 import com.example.tpf_paii_android.R;
 
 public class PerfilActivity extends AppCompatActivity {
 
-    private EditText etNombreUsuario;
+    private TextView tvNombreUser;
     private Button btnCambiarContrasena, btnGestionarPerfil;
 
-    private String nombreUsuario;
+    private String nombreUsuario, tipoUser;
 
 
     @Override
@@ -25,16 +28,34 @@ public class PerfilActivity extends AppCompatActivity {
         setContentView(R.layout.activity_perfil);
         MyApp app = (MyApp) getApplication();
         nombreUsuario = app.getNombreUsuario();
+        tipoUser = app.getTipoUsuario();
 
-        etNombreUsuario = findViewById(R.id.et_nombre_usuario);
+        tvNombreUser = findViewById(R.id.tvNombreUserMOD);
 
         btnCambiarContrasena = findViewById(R.id.btnCambiarContrasena);
         btnGestionarPerfil = findViewById(R.id.btnGestionarPerfil);
 
-        etNombreUsuario.setText(nombreUsuario);
+        tvNombreUser.append(nombreUsuario);
 
         btnCambiarContrasena.setOnClickListener(view -> {
             startActivity(new Intent(PerfilActivity.this, ModificarContrasena.class));
+        });
+
+        btnGestionarPerfil.setOnClickListener(view -> {
+            // Redirigir según el tipo de usuario
+            switch (tipoUser) {
+                case "Estudiante":
+                    startActivity(new Intent(PerfilActivity.this, ModificarEstudiante.class));
+                    break;
+                case "Empresa":
+                    startActivity(new Intent(PerfilActivity.this, ModificarEmpresa.class));
+                    break;
+                case "Tutor":
+                    startActivity(new Intent(PerfilActivity.this, ModificarTutor.class));
+                    break;
+                default:
+                    break;
+            };
         });
 
     }
